8d15fcfb37840b8cd743ef8dbe932eca279a717b
cam <admin.com>
2021-05-28 10:19:34 +0800

    Add dynamic alloc & learn how to use AsRef,Deref

---
+++ alloc/src/lib.rs
@@ -0,0 +1,90 @@
+use std::{alloc::{Layout, alloc, dealloc}, ops::Deref, sync::atomic::Ordering};
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        execute();
+    }
+}
+#[repr(align(8))]
+struct  IVec([u8;8], usize);
+impl IVec{
+    fn new(slice: &[u8]) -> IVec {
+        let mut data = [0_u8;8];
+        // 内存分配大小和对齐方式
+        let layout = Layout::from_size_align(
+            slice.len(), 8
+        ).unwrap();
+        // 分配内存
+        unsafe {
+            let ptr = alloc(layout);
+            // 拷贝slice到新的内存地址
+            std::ptr::copy_nonoverlapping(slice.as_ptr(), ptr, slice.len());
+            // data存放ptr的地址
+            // 'as _' 自动推导出类型
+            // std::ptr::write_unaligned(data.as_mut_ptr() as _, ptr);
+            std::ptr::write_unaligned(data.as_mut_ptr() as *mut *mut u8, ptr);
+        }
+        IVec(data, slice.len())
+    }
+    fn get(&self) -> &[u8]{
+        self.as_ref()
+    }
+    /// 获取指针地址
+    fn remote_ptr(&self) -> *const u8 {
+    unsafe { std::ptr::read(self.0.as_ptr() as *const *const u8) }
+}
+}
+/// 解析,资源释放
+impl Drop for IVec {
+    fn drop(&mut self) {
+                let layout = Layout::from_size_align(
+                    self.1 ,
+                    8,
+                )
+                .unwrap();
+
+                std::sync::atomic::fence(Ordering::Acquire);
+                unsafe {
+                    dealloc(self.remote_ptr() as *mut u8, layout);
+                }
+            }
+    }
+/// Vec<u8>.into() 会执行IVec::from(Vec<u8>)
+impl From<Vec<u8>> for IVec {
+    fn from(slice: Vec<u8>) -> Self{
+        IVec::new(&slice)
+    }
+}
+/// 解引用
+/// &IVec to &[u8]
+impl Deref for IVec {
+    type Target = [u8];
+
+    #[inline]
+    fn deref(&self) -> &[u8] {
+            unsafe {
+                let data_ptr = self.remote_ptr();
+                // 读取内存数据
+                std::slice::from_raw_parts(data_ptr, self.1)
+            }
+    }
+}
+/// 引用
+impl AsRef<[u8]> for IVec {
+    #[inline]
+    fn as_ref(&self) -> &[u8] {
+        self
+    }
+}
+pub fn execute(){
+  let mut key = vec![];
+  key.extend_from_slice(b"kkkkkkkkkkkkkkkkk");
+  let b = key.clone();
+  // from和into结果是一样的
+  // into里面也是执行from
+  //   let key_i: IVec = IVec::from(key);
+  let key_i: IVec = key.into();
+  assert_eq!(&b, key_i.get());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod daemon;
 mod lib;
+use std::ops::Deref;
+
 use daemon::demo;
 use futures::executor;
 use hello_rust::rc_refcell_test;
@@ -17,20 +19,62 @@ use lib::{enum_struct, image, oop};
 // "Hello, world!"
 // }
 // #[rocket::main]
+
+fn hello(a: &str) {
+    println!("{}", a)
+}
+use std::io::{self, BufRead};
+use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
+use std::time::Duration;
+
+fn run() {
+    let (tx, rx) = mpsc::channel();
+    let thread = thread::spawn(move || loop {
+        thread::sleep(Duration::from_millis(1));
+        match rx.recv() {
+            Ok(msg) => {
+                if msg == "stop" {
+                    break;
+                }
+                println!("do task => {}", msg);
+            }
+            Err(_recverror) => {
+                break;
+            }
+        }
+    });
+    let stdin = io::stdin();
+    loop {
+        let mut line = String::new();
+        let _ = stdin.lock().read_line(&mut line);
+        match &*line {
+            "stop\n" => {
+                tx.send(line.trim().to_string()).unwrap();
+                break;
+            }
+            _ => tx.send(line.trim().to_string()).unwrap(),
+        }
+    }
+    if let Ok(_) = thread.join() {};
+    println!("Main thread: the associated thread was finished.")
+}
 fn main() {
     // guess_game::execute();
-    data_type::execute();
-    functions::execute();
-    control_flow::execute();
-    struction::execute();
-    image::execute();
-    enum_struct::execute();
-    oop::execute();
-    // async_test::execute();
-    macro_test::execute();
-    // rc_refcell_test::execute();
-    time_test::execute();
-    demo::execute(false);
+    // run();
+    // data_type::execute();
+    // functions::execute();
+    // control_flow::execute();
+    // struction::execute();
+    // image::execute();
+    // enum_struct::execute();
+    // oop::execute();
+    // // async_test::execute();
+    // macro_test::execute();
+    // // rc_refcell_test::execute();
+    // time_test::execute();
+    // demo::execute(false);
     // http_server::execute();
-    data_structure::execute();
+    // data_structure::execute();
+    alloc::execute();
 }
